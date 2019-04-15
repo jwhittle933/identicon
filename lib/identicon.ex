773bb9426462919b124252bfd482862be160103b
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-15 09:28:27 -0400

    some updates

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,5 +1,4 @@
 defmodule Identicon do
-
   def main(input) do
     input
     |> hash_input
@@ -10,51 +9,62 @@ defmodule Identicon do
     |> draw_image
     |> save_image(input)
   end
-  
+
+  @doc """
+    save_image func wrapper for saving file to os
+  """
   def save_image(image, input) do
     File.write("#{input}.png", image)
   end
 
-  def draw_image(%Identicon.Image{ color: color, pixel_map: pixel_map}) do
+  @doc """
+    draw_image func wrapper for creating png
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
 
-    Enum.each pixel_map, fn({start, stop}) -> 
+    Enum.each(pixel_map, fn {start, stop} ->
       :egd.filledRectangle(image, start, stop, fill)
-    end
-    :egd.render(image) 
+    end)
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map grid, fn({_code, index}) -> 
-      horizontal = rem(index, 5) * 50 
-      vertical = div(index, 5) * 50 
-      top_left = {horizontal, vertical}
-      bottom_right = {horizontal + 50, vertical + 50}
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
 
-      {top_left, bottom_right}
-    end
+        {top_left, bottom_right}
+      end)
 
-    %Identicon.Image{ image | pixel_map: pixel_map }
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn({code, _index}) ->  
-      rem(code, 2) == 0
-    end
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
 
-    %Identicon.Image{ image | grid: grid}
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    grid = 
+    grid =
       hex
-      |> Enum.chunk(3) #Compiler warns that Enum.chunk is deprecated, use Enum.chunk_every instead, but causes error
-      |> Enum.map(&mirror_row/1) # "&" passes reference to function without invoking the function
-      |> List.flatten
-      |> Enum.with_index
+      # Compiler warns that Enum.chunk is deprecated, use Enum.chunk_every instead, but causes error
+      |> Enum.chunk(3)
+      # "&" passes reference to function without invoking the function
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
 
-    %Identicon.Image{ image | grid: grid}
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
@@ -72,13 +82,13 @@ defmodule Identicon do
   ## Examples
     iex> Identicon.hash_input
     :ok
-  
+
   """
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |>:binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
-
 end
